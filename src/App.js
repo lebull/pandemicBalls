d6377173aadf7e37f1d4586b58e69c7f83fda07e
lebull <bulldogboy912000@yahoo.com>
2020-05-04 09:26:16 -0500

    Refactor fullpage list

--- src/App.js
+++ src/App.js
@@ -1,37 +1,10 @@
 import React from 'react';
 import './App.scss';
-import ReactFullpage from '@fullpage/react-fullpage';
-import { EventCollection } from './components/events/Event';
-import { pandemics } from './pandemics';
-
-const validPandemic= (event) => {
-    return event.name && event.deaths && event.minDeaths && event.minDeaths > 100000;
-}
+import { FullPageList } from './components/layout/FullPageList'
 
 const App = () => {
     return (
-        <ReactFullpage
-            className={"TimeLine"}
-            licenseKey={"0822866D-6AE240B1-BFCD182C-DA43DAE6"}
-            bigSectionsDestination={"top"}
-            scrollingSpeed={1000}
-            navigation={false}
-            // navigationTooltips={navigationTooltips}
-            recordHistory={false}
-            // anchors={anchors}
-            render={
-                ({ state, fullpageApi }) => {
-                    return(
-                        <div data-testid="timeline">
-                            <ReactFullpage.Wrapper>
-                                <EventCollection events={pandemics.filter(validPandemic)} renderMode="threedee"/>
-                            </ReactFullpage.Wrapper>
-                        </div>
-                    )
-                }
-            }
-
-        />
+      <FullPageList/>
     );
 }
 export default App;
