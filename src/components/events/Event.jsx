23953d536a8fe039a14a5183f004baa07e886c34
lebull <bulldogboy912000@yahoo.com>
2020-05-16 23:30:56 -0500

    init?

--- src/components/events/Event.jsx
+++ src/components/events/Event.jsx
@@ -5,12 +5,11 @@ export const RenderMode = {
     threedee: "threedee"
 }
 
-export const EventCollection = (props) =>
-    <div>
-        {props.events.map((event, index) =>
-            <Event key={index} event={event} renderMode={props.renderMode} />
-        )}
-    </div>;
+export const EventCollection = (props) => (
+    props.events.map((event, index) =>
+        <Event key={index} event={event} renderMode={props.renderMode} />
+    )
+);
 
 const Event = (props) => {
     return (
@@ -27,9 +26,9 @@ const EventInfo = (props) => {
     return (
         <div className="Info">
             <h1>{props.event.name}</h1>
-            <p>{props.event.date}</p>
+            {/* <p>{props.event.date}</p>
             <p>{props.event.location} - {props.event.disease}</p>
-            <p>{props.event.deaths} deaths</p>
+            <p>{props.event.deaths} deaths</p> */}
         </div>
     )
 }
@@ -58,8 +57,9 @@ const EventCircle = (props) => {
     }
 
     if(props.event.maxDeaths){
+        let currentEventClass = props.currentEvent ? "currentEvent" : "";
         return (
-            <div className="Circle transparent" style={scaleSize(props.event.maxDeaths)}>
+            <div className={`Circle transparent ${currentEventClass}`} style={scaleSize(props.event.maxDeaths)}>
                 <div className="Circle" style={scaleSize(props.event.minDeaths)} />
             </div>
         );
